66cb150c2a9402609e3e555b906e94109a1b9907
Miguel Saldivar <miguel.saldivar22@hotmail.com>
2017-04-28 15:24:55 -0700

    :thinking emoji

--- trackpy.py
+++ trackpy.py
@@ -3,10 +3,18 @@ import imutils
 import cv2
 
 camera = cv2.VideoCapture('videos/green_screen.mp4')
-cv2
+
+# grab the color green
+green_lower = (29, 86, 6)
+green_upper = (64, 255, 255)
 
 while True:
     (grabbed, frame) = camera.read()
+
+
+    frame = imutils.resize(frame, width=600)
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     # this is what grabs the next frame of the video
     cv2.imshow("Frame", frame)
